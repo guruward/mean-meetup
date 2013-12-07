8717311ead2862afe514cb56117ddaf2ac21db7e
Kalarrs Topham <k-dawg-klark@hotmail.com>
2013-12-06 23:48:45 -0700

    #time 5m
    * Fixed typo in refactor of /api/v1/

--- routes.js
+++ routes.js
@@ -10,21 +10,21 @@ exports = module.exports = function(app){
 
     // users
     app.get('/api/v1/users',require('./api/users').list);
-    app.get('/api/v1//users/:userId',require('./api/users').get);
-    app.post('/api/v1//users',require('./api/users').post) ;
-    app.put('/api/v1//users',require('./api/users').put) ;
-    app.delete('/api/v1//users/:userId',require('./api/users').delete) ;
+    app.get('/api/v1/users/:userId',require('./api/users').get);
+    app.post('/api/v1/users',require('./api/users').post) ;
+    app.put('/api/v1/users',require('./api/users').put) ;
+    app.delete('/api/v1/users/:userId',require('./api/users').delete) ;
 
     // Communications
-    app.get('/api/v1//communication/',require('./api/communications').list) ;
-    app.get('/api/v1//communication/:commId',require('./api/communications').get) ;
-    app.post('/api/v1//communication',require('./api/communications').post) ;
-    app.put('/api/v1//communication',require('./api/communications').post) ;
+    app.get('/api/v1/communication/',require('./api/communications').list) ;
+    app.get('/api/v1/communication/:commId',require('./api/communications').get) ;
+    app.post('/api/v1/communication',require('./api/communications').post) ;
+    app.put('/api/v1/communication',require('./api/communications').post) ;
     app.delete('/api/v1/communication/:commId',require('./api/communications').delete) ;
 
     // contacts
     app.get('/api/v1/contact/',require('./api/contacts').list) ;
-    app.get('/api/v1//contact/:commId',require('./api/contacts').get) ;
+    app.get('/api/v1/contact/:commId',require('./api/contacts').get) ;
     app.post('/contact/',require('./api/contacts').post) ;
     app.put('/contact/',require('./api/contacts').put) ;
     app.delete('/contact/:contactId',require('./api/contacts').delete) ;
